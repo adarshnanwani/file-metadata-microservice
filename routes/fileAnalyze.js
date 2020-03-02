2a6a419aec11d53379c69cc4e060bcb5ec741c59
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-03-02 13:25:58 +0530

    Implemented the API.

--- routes/fileAnalyze.js
+++ routes/fileAnalyze.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const fileUpload = require('express-fileupload');
+
+router.use(
+  fileUpload({
+    createParentPath: true
+  })
+);
 
 router.route('/').post((req, res) => {
-  res.json({ success: true });
+  try {
+    if (!req.files) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Please upload a file.' });
+    }
+
+    let file = req.files.upfile;
+
+    res
+      .status(200)
+      .json({ name: file.name, type: file.mimetype, size: file.size });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'An error occurred.' });
+  }
 });
 
 module.exports = router;
